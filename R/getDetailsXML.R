4a27d62eef96c7efc580d205ef84f282bf044eb7
JARS3N <JARS3N@users.noreply.github.com>
2016-11-07 15:15:42 -0500

    Create getDetailsXML.R

---
+++ R/getDetailsXML.R
@@ -0,0 +1,10 @@
+getDetailsXML<-function(DIR){
+require(parallel);require(dplyr);
+FLS<- list.files(DIR,recursive=T,pattern='details.xml',full.names = T);
+size.of.list <- length(FLS);
+cl <- makeCluster( min(size.of.list, detectCores()) );
+Out<-parallel::parLapply(cl=cl,FLS,sortMV::MVdetails) %>% 
+  bind_rows()
+stopCluster(cl);
+Out
+}
