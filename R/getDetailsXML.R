6c473c221904dd83ecb6c89cc1de1fa1c4dba503
JARS3N <JARS3N@users.noreply.github.com>
2017-03-10 12:30:36 -0500

    Update getDetailsXML.R

--- R/getDetailsXML.R
+++ R/getDetailsXML.R
@@ -1,5 +1,6 @@
 getDetailsXML<-function(DIR){
 require(parallel);require(dplyr);
+require(xml2);
 FLS<- list.files(DIR,recursive=T,pattern='details.xml',full.names = T);
 size.of.list <- length(FLS);
 cl <- makeCluster( min(size.of.list, detectCores()) );
