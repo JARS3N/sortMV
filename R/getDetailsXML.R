b65f520a3a9684be6a576b0207217c14202a58d7
JARS3N <JARS3N@users.noreply.github.com>
2017-03-29 13:43:44 -0400

    Update getDetailsXML.R

--- R/getDetailsXML.R
+++ R/getDetailsXML.R
@@ -5,6 +5,7 @@ FLS<- list.files(DIR,recursive=T,pattern='details.xml',full.names = T);
 size.of.list <- length(FLS);
 cl <- makeCluster( min(size.of.list, detectCores()) );
 Out<-parallel::parLapply(cl=cl,FLS,sortMV::MVdetails) %>%
+  .[!grepl("FAILURE",.)] %>%
   bind_rows()
 stopCluster(cl);
 Out
